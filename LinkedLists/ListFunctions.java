48488b51081fb0471f2ca5730d06ebb286ff7606
Meet953 <meetmehta953@gmail.com>
2019-02-13 09:19:07 +0000

    Add files via upload

---
+++ LinkedLists/ListFunctions.java
@@ -0,0 +1,65 @@
+package LinkedLists;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+
+
+class ListFunctions{	
+	
+	public static void printlist(LinkedList<Node> list){
+	    Node temp;
+	    Iterator<Node> iter= list.iterator();
+	    System.out.println("\nList:");
+	    System.out.print("HEAD->");
+	    while(iter.hasNext()) {
+	    	temp=(Node)iter.next();
+	        temp.print();
+	        }
+	        
+	     System.out.print("NULL");   
+	    }
+	
+	
+	public static boolean searchName(LinkedList<Node> list, String name){
+		
+		
+		 Node temp;
+		    Iterator<Node> iter= list.iterator();
+		    System.out.println("\nList:");
+		    System.out.print("HEAD->");
+		    while(iter.hasNext()) {
+		    	temp=(Node)iter.next();
+		        if(temp.readname().equals(name) ) {
+		        	return true;
+		        }
+		        }
+		        
+		    
+		     
+	    return false;
+	    }
+	
+	
+	public static int countOccurs(LinkedList<Node> list, String name){
+	    int count = 0;
+		
+	    Node temp;
+	    Iterator<Node> iter= list.iterator();
+	    System.out.println("\nList:");
+	    System.out.print("HEAD->");
+	    while(iter.hasNext()) {
+	    	temp=(Node)iter.next();
+	        if(temp.readname().equals(name) ) {
+	        	count++;
+	        }
+	        }
+	    
+		return count;
+	    }
+	
+	
+   }
+
+
+   
